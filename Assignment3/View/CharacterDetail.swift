9c8072fc6f4234d9ea2e0551dad5f83f2061d861
Kyle Zimmerman <kzdesignsios@gmail.com>
2024-03-31 13:27:07 -0700

    added scrollview to character detail view, this improves the users experience in landscape mode

--- Assignment3/View/CharacterDetail.swift
+++ Assignment3/View/CharacterDetail.swift
@@ -12,41 +12,47 @@ struct CharacterDetail: View {
     var character : Character
     
     var body: some View {
-        VStack (alignment: .leading, spacing: 10) {
-            Text(character.name)
-                .font(.system(size: 32))
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                .padding(.bottom)
-                .padding(.leading)
-            
-            CharacterStatus(status: character.status)
-                .font(.system(size: 22))
-                .padding(.horizontal)
-            
-            Text("Species: \(character.species)")
-                .font(.system(size: 22))
-                .padding(.horizontal)
-            
-            if !character.type.isEmpty {
-                Text("Type: \(character.type)")
-                    .font(.system(size: 18))
-                    .padding(.horizontal)
-            }
-            
-            if !character.gender.isEmpty {
-                Text("Gender: \(character.gender)")
-                    .font(.system(size: 18))
-                    .padding(.horizontal)
-            }
-           
-            if !character.origin.name.isEmpty {
-                Text("Origin: \(character.origin.name)")
-                    .font(.system(size: 18))
-                    .padding(.horizontal)
+        ScrollView {
+            VStack (alignment: .leading, spacing: 10) {
+                Text(character.name)
+                    .font(.system(size: 32))
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                     .padding(.bottom)
+                    .padding(.leading)
+                
+                CharacterStatus(status: character.status)
+                    .font(.system(size: 22))
+                    .padding(.horizontal)
+                
+                Text("Species: \(character.species)")
+                    .font(.system(size: 22))
+                    .padding(.horizontal)
+                
+                if !character.type.isEmpty {
+                    Text("Type: \(character.type)")
+                        .font(.system(size: 18))
+                        .padding(.horizontal)
+                }
+                
+                if !character.gender.isEmpty {
+                    Text("Gender: \(character.gender)")
+                        .font(.system(size: 18))
+                        .padding(.horizontal)
+                }
+               
+                if !character.origin.name.isEmpty {
+                    Text("Origin: \(character.origin.name)")
+                        .font(.system(size: 18))
+                        .padding(.horizontal)
+                        .padding(.bottom)
+                }
+                
+                CharacterImageView(imageURL: character.image);
             }
-            
-            CharacterImageView(imageURL: character.image);
+            .frame(maxWidth: .infinity)
+            .padding(.top, 20)
         }
+       
+
     }
 }
